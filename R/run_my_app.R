c04c4471e1d96196a773e0f273c0f778549a17d0
Clarence Williams <clarencew3@gmail.com>
2021-04-07 17:03:03 -0400

    Cleaned up files to build basic structure for package and shiny app.

---
+++ R/run_my_app.R
@@ -0,0 +1,34 @@
+#' @export
+#' @importFrom  utils View
+run_my_app  <-
+  function(app_name = NULL,
+           theme = 'flatly',
+           width = '100%',
+           height = '800px',
+           more_opts = list(NA),
+           launch.browser = TRUE,...)
+  {
+
+    valid_apps <- list.files(system.file("apps", package = "DecisionTool"))
+
+    valid_apps_df <- data.frame(valid_apps)
+    colnames(valid_apps_df) <- 'Valid apps'
+
+    if (missing(app_name) || !nzchar(app_name) || !app_name %in% valid_apps) {
+
+      stop(paste0('Please run `run_my_app()` with a valid app as an argument.\n',
+                  "See table for Valid apps"),
+           utils::View(valid_apps_df),
+           call. = FALSE)
+  }
+
+
+    dir <- system.file('apps', app_name, package = 'DecisionTool')
+
+
+    shiny::shinyAppDir(appDir = dir,
+                       options = list(height = height,
+                                      width = width,
+                                      launch.browser = launch.browser,...))
+
+  }
