dae2ce8ef563cc3f3266168c5688d7d29e9d2f43
Clarence Williams <clarencew3@gmail.com>
2021-04-29 14:01:18 -0400

    Add code block for unique problem

--- inst/Apps/SliderTest/ui.R
+++ inst/Apps/SliderTest/ui.R
@@ -6,8 +6,9 @@ ui <- fluidPage(title = 'Test Slider',
                             selectInput("SelectIndicator", "Select Criteria", choices = criteria),
 
                             sliderInput('SelectIndicatorWeight', 'Select Criteria Weight',
-                                        min=0, max=4, value= main.weights[1,1], # initial value
-                                        step=0.1, round=0)
+                                        min=0, max=1, value= main.weights[1,1], # initial value
+                                        step=0.05, round=0),
+                            actionButton("reset", "Reset")
 
 
                   ),
